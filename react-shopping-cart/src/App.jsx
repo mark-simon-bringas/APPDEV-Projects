4fb718ee7f9515bfb6e942117288b5601bad1499
bringas_m <bringas_m@f112.lan>
2024-10-18 13:12:16 +0800

    Added Footer

--- react-shopping-cart/src/App.jsx
+++ react-shopping-cart/src/App.jsx
@@ -8,12 +8,14 @@ Customer()  Admin()
 
 import Login from './Login'
 import Shop from './Shop'
+import Footer from './Footer'
 import './App.css'
 
 function App() {
   return (
     <>
       <Login />
+      <Footer />
     </>
   )
 }
---
+++ react-shopping-cart/src/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+        <>
+            <footer>
+                <hr />
+                <p>&copy; {new Date().getFullYear()} My Shopping Cart Website || 
+                Written by: Mark Simon Z. Bringas</p>
+            </footer>
+        </>
+    )
+}
