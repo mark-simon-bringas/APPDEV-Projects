376f271e3c9a7761493882768b348fdc2417a273
bringas_m <bringas_m@f112.lan>
2024-12-04 11:55:17 +0800

    Updated components

--- react-keystaxx/src/components/Employee.jsx
+++ react-keystaxx/src/components/Employee.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useLocation } from "react-router-dom"
 
 export default function Employee() {
@@ -10,17 +9,4 @@ export default function Employee() {
             <p>Employee page</p>
         </>
     )
-=======
-import { useLocation } from "react-router-dom"
-
-export default function Employee() {
-    const location = useLocation()
-    const employee = location.state.employee
-
-    return (
-        <>
-            <p>Employee page</p>
-        </>
-    )
->>>>>>> 2cb83542e095037af5af94eec9b81b670dbdc6b5
 }
